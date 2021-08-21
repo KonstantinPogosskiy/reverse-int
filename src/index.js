5b164724a5901b299f598b6ac54462623e02b1a0
KonstantinPogosskiy <darcus666@yandex.ru>
2021-08-21 13:35:16 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let num = n.toString().split('')
+    for(let i = 0; i < num.length; i++) {
+        if(num[i] === '-') {
+            let num2 = num.shift()
+        }
+    }
+
+    return +num.reverse().join('')
 }
